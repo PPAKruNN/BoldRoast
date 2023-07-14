66b77301b25e4f13e4bbbcfe118de2563799ab09
rafaelanobre <raafanobre@gmail.com>
2023-07-13 22:46:21 -0300

    :art: Style: menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,57 +7,66 @@ import icon2 from "../images/icon2.png"
 export default function FixedMenu() {
 
     return (
-        <MenuContainer>
-            <ImageText>
-                <img src={image}></img>
-                <img src={text}></img>
-            </ImageText>
+        <Menu>
+            <MenuContainer>
+                <ImageText>
+                    <img src={image}></img>
+                    <img src={text}></img>
+                </ImageText>
 
-            <input placeholder="Pesquisar"/>
+                <input placeholder="Pesquisar"/>
 
-            <Icons>
-                <img src={icon1}></img>
-                <img src={icon2}></img>
-            </Icons>
-        </MenuContainer>
+                <Icons>
+                    <img src={icon1}></img>
+                    <img src={icon2}></img>
+                </Icons>
+            </MenuContainer>
+        </Menu>
     )
 }
 
-const MenuContainer = styled.div`
+const Menu = styled.div`
 height: 143px;
 width: 100%;
 position: fixed;
 display: flex;
-justify-content: space-between;
-align-items: center;
+justify-content: center;
 z-index: 2;
 top: 0px;
 left: 0px;
-
-input{
-    width: 337px;
-    height: 35px;
-    border-radius: 10px; 
-    background-color: #EBEBEB;
-    border: 1px solid #013743;
-    padding: 20px;
-    
-    ::placeholder {
-        color: #424B5A;
-        font-size: 14px;
-    }
-}
 `
 
+const MenuContainer = styled.div`
+    width: 1100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    input{
+        width: 337px;
+        height: 35px;
+        border-radius: 10px; 
+        background-color: #EBEBEB;
+        border: 1px solid #013743;
+        padding: 20px;
+        
+        ::placeholder {
+            color: #424B5A;
+            font-size: 14px;
+        }
+    }
+    `
+
 const ImageText = styled.div`
-width: 227px;
-height: 129px;
+width: 200px;
+height: 105px;
 display: flex;
 ` 
 
 const Icons = styled.div`
-width:76px;
+width:200px;
 height: 33px;
 display: flex;
-justify-content: space-around;
+justify-content: center;
+gap: 15px;
 `
